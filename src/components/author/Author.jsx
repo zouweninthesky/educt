503667e443d7a7792d19d65afa5f80c3d9ba3fce
Zouwen <zouweninthesky@yandex.ru>
2021-09-20 16:56:35 +0300

    create Author page

--- src/components/author/Author.jsx
+++ src/components/author/Author.jsx
@@ -1,8 +1,19 @@
 import React from "react";
 import "./Author.scss";
 
+import Header from "../common/Header/Header";
+import Hub from "../common/Hub/Hub";
+
 const Author = () => {
-  return <div>Я автор</div>;
+  return (
+    <>
+      <h1 className="visually-hidden">
+        Меню выбора сценариев для редактирования
+      </h1>
+      <Header />
+      <Hub isAuthor={true} />
+    </>
+  );
 };
 
 export default Author;
