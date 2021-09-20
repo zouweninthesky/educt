9c5dda0f0a7c6b547978ee627ac8512e90d7240a
Zouwen <zouweninthesky@yandex.ru>
2021-09-20 16:00:26 +0300

    create Overview

--- src/components/editor/Editor.jsx
+++ src/components/editor/Editor.jsx
@@ -1,13 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+import Icon from "../common/Icon/Icon";
 import Viewbox from "../common/Viewbox/Viewbox";
+import Overview from "./Overview/Overview";
 import Tools from "./Tools/Tools";
 
+import "./Editor.scss";
+
+const HEADER_TOOLS_ON = "Все слайды";
+const HEADER_TOOLS_OFF = "Вернуться к списку";
+
 const Editor = () => {
+  // temp
+  const [toolsShown, setTools] = useState(true);
+  const onArrowClick = () => {
+    setTools(false);
+  };
+
+  // will depend on currentSlide being not null
+  const headerContent = toolsShown ? HEADER_TOOLS_ON : HEADER_TOOLS_OFF;
+  const arrow = () => {
+    if (toolsShown)
+      return (
+        <button className="editor__arrow-button" onClick={onArrowClick}>
+          <Icon id="arrow-left" width="24" />
+        </button>
+      );
+    return (
+      <Link to="/author" className="editor__arrow-button">
+        <Icon id="arrow-left" width="24" />
+      </Link>
+    );
+  };
+
   return (
     <main className="editor">
       <Viewbox mod={"editor"} />
 
-      <Tools />
+      <section className="editor__panel">
+        <div className="editor__header-wrapper">
+          {arrow()}
+          <h2 className="editor__header">{headerContent}</h2>
+        </div>
+        {toolsShown ? <Tools /> : <Overview />}
+      </section>
     </main>
   );
 };
---
+++ src/components/editor/Overview/Overview.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+
+import Icon from "../../common/Icon/Icon";
+import Thumbnail from "../../../static/img/test/temp-slide-thumbnail.jpg";
+
+import "./Overview.scss";
+
+const Overview = () => {
+  return (
+    <div className="overview">
+      <div className="overview__item">
+        <button className="overview__button">
+          <Icon id="info" width="64" />
+          <h3 className="overview__title">Общие настройки</h3>
+        </button>
+      </div>
+      <ul className="overview__list">
+        <li className="overview__item">
+          <button className="overview__button">
+            <img src={Thumbnail} width="110" height="62" alt="thumbnail" />
+            <h3 className="overview__title">Слайд 1</h3>
+          </button>
+        </li>
+        <li className="overview__item">
+          <button className="overview__button">
+            <img src={Thumbnail} width="110" height="62" alt="thumbnail" />
+            <h3 className="overview__title">Слайд 2</h3>
+          </button>
+        </li>
+        <li className="overview__item">
+          <button className="overview__button">
+            <img src={Thumbnail} width="110" height="62" alt="thumbnail" />
+            <h3 className="overview__title">Слайд 3</h3>
+          </button>
+        </li>
+        <li className="overview__item">
+          <button className="overview__button">
+            <img src={Thumbnail} width="110" height="62" alt="thumbnail" />
+            <h3 className="overview__title">Слайд 4</h3>
+          </button>
+        </li>
+        <li className="overview__item">
+          <button className="overview__button">
+            <img src={Thumbnail} width="110" height="62" alt="thumbnail" />
+            <h3 className="overview__title">Слайд 5</h3>
+          </button>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Overview;
