6603cc6b8be6b231e8b32add55c8e1bafbf713af
Zouwen <zouweninthesky@yandex.ru>
2021-10-01 13:46:13 +0300

    add .idea to gitignore

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -6,10 +6,9 @@ import Authentication from "../common/Authentication/Authentication";
 import Auth from "../../store/Auth";
 
 const Login = () => {
-
   const initialState = {
     login: undefined,
-    password: undefined
+    password: undefined,
   };
 
   const [loginState, setLoginState] = useState(initialState.login);
@@ -23,22 +22,25 @@ const Login = () => {
   return (
     <Authentication>
       <h1 className="auth__header">Войти</h1>
-      <p className="auth__create-acc">
-        Ещё не с нами?
-        <Link to="/user">Создать аккаунт</Link>
-      </p>
       <form onSubmit={(e) => signIn(e)} className="auth__form">
         <div className="auth__input">
           <label htmlFor="email">Электронная почта</label>
-          <input type="email" id="email" value={loginState} onInput={(e) => setLoginState(e.target.value)} />
+          <input
+            type="email"
+            id="email"
+            value={loginState}
+            onInput={(e) => setLoginState(e.target.value)}
+          />
         </div>
         <div className="auth__input">
           <label htmlFor="password">Пароль</label>
-          <input type="password" id="password" value={passwordState} onInput={(e) => setPasswordState(e.target.value)} />
+          <input
+            type="password"
+            id="password"
+            value={passwordState}
+            onInput={(e) => setPasswordState(e.target.value)}
+          />
         </div>
-        <Link className="auth__forgot-password" to="/user">
-          Забыли пароль?
-        </Link>
         <button className="auth__submit button" type="submit">
           Войти в аккаунт &#8594;
         </button>
