973262b5c3a6ae6bc393d82803ce55d73eb54d48
Zouwen <zouweninthesky@yandex.ru>
2021-10-13 18:04:38 +0300

    add scrollbar to hubs

---
+++ src/utils/UserScriptsServiceFake.js
@@ -0,0 +1,549 @@
+class UserScriptsServiceFake {
+  data = [
+    {
+      UID: 24,
+      title: "Выгрузка табеля учета рабочего времени",
+      description:
+        "Тут будет описание для этого сцернария. Пользователь только почитать может, а редактор изменить еще. Надо придумать ограничение по количеству симоволов.",
+      date: "2021-09-23T08:57:20.903557Z",
+      timeLength: "1",
+      slides: [
+        {
+          img: "",
+          description: "Текст 1",
+        },
+        {
+          img: "",
+          description: "Текст 2",
+        },
+        {
+          img: "",
+          description: "Текст 3",
+        },
+        {
+          img: "",
+          description: "Текст 4",
+        },
+        {
+          img: "",
+          description: "Текст 5",
+        },
+      ],
+    },
+    {
+      UID: 11,
+      title: "Сценарий №32",
+      description: "Они из Перми",
+      date: "2021-09-23T08:57:20.903557Z",
+      timeLength: "1",
+      slides: [
+        {
+          img: "",
+          description: "Текст 1",
+        },
+        {
+          img: "",
+          description: "Текст 2",
+        },
+        {
+          img: "",
+          description: "Текст 3",
+        },
+        {
+          img: "",
+          description: "Текст 4",
+        },
+      ],
+    },
+    {
+      UID: 54,
+      title: "Слава чудесный человек",
+      description: "И хуй у него здоровенный",
+      date: "2021-09-28T08:57:20.903557Z",
+      timeLength: "12",
+      slides: [
+        {
+          img: "",
+          description: "Текст 1",
+        },
+        {
+          img: "",
+          description: "Текст 2",
+        },
+        {
+          img: "",
+          description: "Текст 3",
+        },
+        {
+          img: "",
+          description: "Текст 4",
+        },
+        {
+          img: "",
+          description: "Текст 4",
+        },
+        {
+          img: "",
+          description: "Текст 4",
+        },
+        {
+          img: "",
+          description: "Текст 4",
+        },
+        {
+          img: "",
+          description: "Текст 4",
+        },
+        {
+          img: "",
+          description: "Текст 5",
+        },
+      ],
+    },
+    {
+      UID: 22,
+      title: "Я обожаю реакт",
+      description: "Дайте денег",
+      date: "2021-09-11T08:57:20.903557Z",
+      timeLength: "1",
+      slides: [
+        {
+          img: "",
+          description: "Текст 1",
+        },
+        {
+          img: "",
+          description: "Текст 2",
+        },
+        {
+          img: "",
+          description: "Текст 3",
+        },
+        {
+          img: "",
+          description: "Текст 4",
+        },
+        {
+          img: "",
+          description: "Текст 5",
+        },
+      ],
+    },
+    {
+      UID: 112,
+      title: "Сценарий №32",
+      description: "Они из Перми",
+      date: "2020-09-11T08:57:20.903557Z",
+      timeLength: "1",
+      slides: [
+        {
+          img: "",
+          description: "Текст 1",
+        },
+        {
+          img: "",
+          description: "Текст 2",
+        },
+        {
+          img: "",
+          description: "Текст 3",
+        },
+        {
+          img: "",
+          description: "Текст 4",
+        },
+        {
+          img: "",
+          description: "Текст 5",
+        },
+      ],
+    },
+    {
+      UID: 51,
+      title: "Слава чудесный человек",
+      description: "И хуй у него здоровенный",
+      date: "2020-09-23T08:57:20.903557Z",
+      timeLength: "1",
+      slides: [
+        {
+          img: "",
+          description: "Текст 1",
+        },
+        {
+          img: "",
+          description: "Текст 2",
+        },
+        {
+          img: "",
+          description: "Текст 3",
+        },
+        {
+          img: "",
+          description: "Текст 4",
+        },
+        {
+          img: "",
+          description: "Текст 5",
+        },
+      ],
+    },
+    {
+      UID: 52,
+      title: "Слава чудесный человек",
+      description: "И хуй у него здоровенный",
+      date: "2021-09-21T08:57:20.903557Z",
+      timeLength: "1",
+      slides: [
+        {
+          img: "",
+          description: "Текст 1",
+        },
+        {
+          img: "",
+          description: "Текст 2",
+        },
+        {
+          img: "",
+          description: "Текст 3",
+        },
+        {
+          img: "",
+          description: "Текст 4",
+        },
+        {
+          img: "",
+          description: "Текст 5",
+        },
+      ],
+    },
+    {
+      UID: 53,
+      title: "Слава чудесный человек",
+      description: "И хуй у него здоровенный",
+      date: "2021-08-23T08:57:20.903557Z",
+      timeLength: "1",
+      slides: [
+        {
+          img: "",
+          description: "Текст 1",
+        },
+        {
+          img: "",
+          description: "Текст 2",
+        },
+        {
+          img: "",
+          description: "Текст 3",
+        },
+        {
+          img: "",
+          description: "Текст 4",
+        },
+        {
+          img: "",
+          description: "Текст 5",
+        },
+      ],
+    },
+    {
+      UID: 29,
+      title: "Я обожаю реакт",
+      description: "Дайте денег",
+      date: "2021-09-23T08:57:20.903557Z",
+      timeLength: "1",
+      slides: [
+        {
+          img: "",
+          description: "Текст 1",
+        },
+        {
+          img: "",
+          description: "Текст 2",
+        },
+        {
+          img: "",
+          description: "Текст 3",
+        },
+        {
+          img: "",
+          description: "Текст 4",
+        },
+        {
+          img: "",
+          description: "Текст 5",
+        },
+      ],
+    },
+    {
+      UID: 29,
+      title: "Я обожаю реакт",
+      description: "Дайте денег",
+      date: "2021-09-23T08:57:20.903557Z",
+      timeLength: "1",
+      slides: [
+        {
+          img: "",
+          description: "Текст 1",
+        },
+        {
+          img: "",
+          description: "Текст 2",
+        },
+        {
+          img: "",
+          description: "Текст 3",
+        },
+        {
+          img: "",
+          description: "Текст 4",
+        },
+        {
+          img: "",
+          description: "Текст 5",
+        },
+      ],
+    },
+    {
+      UID: 29,
+      title: "Я обожаю реакт",
+      description: "Дайте денег",
+      date: "2021-09-23T08:57:20.903557Z",
+      timeLength: "1",
+      slides: [
+        {
+          img: "",
+          description: "Текст 1",
+        },
+        {
+          img: "",
+          description: "Текст 2",
+        },
+        {
+          img: "",
+          description: "Текст 3",
+        },
+        {
+          img: "",
+          description: "Текст 4",
+        },
+        {
+          img: "",
+          description: "Текст 5",
+        },
+      ],
+    },
+    {
+      UID: 29,
+      title: "Я обожаю реакт",
+      description: "Дайте денег",
+      date: "2021-09-23T08:57:20.903557Z",
+      timeLength: "1",
+      slides: [
+        {
+          img: "",
+          description: "Текст 1",
+        },
+        {
+          img: "",
+          description: "Текст 2",
+        },
+        {
+          img: "",
+          description: "Текст 3",
+        },
+        {
+          img: "",
+          description: "Текст 4",
+        },
+        {
+          img: "",
+          description: "Текст 5",
+        },
+      ],
+    },
+    {
+      UID: 29,
+      title: "Я обожаю реакт",
+      description: "Дайте денег",
+      date: "2021-09-23T08:57:20.903557Z",
+      timeLength: "1",
+      slides: [
+        {
+          img: "",
+          description: "Текст 1",
+        },
+        {
+          img: "",
+          description: "Текст 2",
+        },
+        {
+          img: "",
+          description: "Текст 3",
+        },
+        {
+          img: "",
+          description: "Текст 4",
+        },
+        {
+          img: "",
+          description: "Текст 5",
+        },
+      ],
+    },
+    {
+      UID: 29,
+      title: "Я обожаю реакт",
+      description: "Дайте денег",
+      date: "2021-09-23T08:57:20.903557Z",
+      timeLength: "1",
+      slides: [
+        {
+          img: "",
+          description: "Текст 1",
+        },
+        {
+          img: "",
+          description: "Текст 2",
+        },
+        {
+          img: "",
+          description: "Текст 3",
+        },
+        {
+          img: "",
+          description: "Текст 4",
+        },
+        {
+          img: "",
+          description: "Текст 5",
+        },
+      ],
+    },
+    {
+      UID: 29,
+      title: "Я обожаю реакт",
+      description: "Дайте денег",
+      date: "2021-09-23T08:57:20.903557Z",
+      timeLength: "1",
+      slides: [
+        {
+          img: "",
+          description: "Текст 1",
+        },
+        {
+          img: "",
+          description: "Текст 2",
+        },
+        {
+          img: "",
+          description: "Текст 3",
+        },
+        {
+          img: "",
+          description: "Текст 4",
+        },
+        {
+          img: "",
+          description: "Текст 5",
+        },
+      ],
+    },
+    {
+      UID: 29,
+      title: "Я обожаю реакт",
+      description: "Дайте денег",
+      date: "2021-09-23T08:57:20.903557Z",
+      timeLength: "1",
+      slides: [
+        {
+          img: "",
+          description: "Текст 1",
+        },
+        {
+          img: "",
+          description: "Текст 2",
+        },
+        {
+          img: "",
+          description: "Текст 3",
+        },
+        {
+          img: "",
+          description: "Текст 4",
+        },
+        {
+          img: "",
+          description: "Текст 5",
+        },
+      ],
+    },
+    {
+      UID: 29,
+      title: "Я обожаю реакт",
+      description: "Дайте денег",
+      date: "2021-09-23T08:57:20.903557Z",
+      timeLength: "1",
+      slides: [
+        {
+          img: "",
+          description: "Текст 1",
+        },
+        {
+          img: "",
+          description: "Текст 2",
+        },
+        {
+          img: "",
+          description: "Текст 3",
+        },
+        {
+          img: "",
+          description: "Текст 4",
+        },
+        {
+          img: "",
+          description: "Текст 5",
+        },
+      ],
+    },
+    {
+      UID: 29,
+      title: "Я обожаю реакт",
+      description: "Дайте денег",
+      date: "2021-09-23T08:57:20.903557Z",
+      timeLength: "1",
+      slides: [
+        {
+          img: "",
+          description: "Текст 1",
+        },
+        {
+          img: "",
+          description: "Текст 2",
+        },
+        {
+          img: "",
+          description: "Текст 3",
+        },
+        {
+          img: "",
+          description: "Текст 4",
+        },
+        {
+          img: "",
+          description: "Текст 5",
+        },
+      ],
+    },
+  ];
+
+  getUserScripts() {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(this.data);
+      }, 400);
+    });
+  }
+}
+
+export default new UserScriptsServiceFake();
