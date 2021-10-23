2e794d8c32cff7c84f0d5a96c74c1c24a361739d
Zouwen <zouweninthesky@yandex.ru>
2021-10-23 13:20:51 +0300

    add disable for tools

--- src/components/editor/Tools/Tools.jsx
+++ src/components/editor/Tools/Tools.jsx
@@ -6,7 +6,7 @@ import Icon from "../../common/Icon/Icon";
 import { useModal } from "../../common/Modal/ModalContext";
 import {
   MODAL_DELETE_STEP_ID,
-  MODAL_COMMENT_ID
+  MODAL_COMMENT_ID,
 } from "../../../utils/constants/modals";
 import { observer } from "mobx-react-lite";
 
@@ -18,14 +18,22 @@ const Tools = observer(({ data }) => {
       <ul className="tools__buttons-list">
         <li className="tools__button-item">
           <div className="tools__buttons-wrapper">
-            <button className="tools__navigation-button" type="button" onClick={() => data.prevStep()}>
+            <button
+              className="tools__navigation-button"
+              type="button"
+              onClick={() => data.prevStep()}
+            >
               <Icon id="angle-left" width="24" />
             </button>
             <p className="tools__navigation-count">
               <span>{data.currentStepNumber + 1}</span>
               <span>из {data.steps.length}</span>
             </p>
-            <button className="tools__navigation-button" type="button" onClick={() => data.nextStep()}>
+            <button
+              className="tools__navigation-button"
+              type="button"
+              onClick={() => data.nextStep()}
+            >
               <Icon id="angle-right" width="24" />
             </button>
           </div>
@@ -41,7 +49,11 @@ const Tools = observer(({ data }) => {
           </button>
         </li>
         <li className="tools__button-item">
-          <button className="tools__button" type="button" onClick={() => data.setActionMode()}>
+          <button
+            className="tools__button"
+            type="button"
+            onClick={() => data.setActionMode()}
+          >
             <Icon id="action" width="64" />
             <h3 className="tools__button-title">Действие</h3>
           </button>
@@ -53,7 +65,11 @@ const Tools = observer(({ data }) => {
           </button>
         </li>
         <li className="tools__button-item">
-          <button className="tools__button" type="button" onClick={() => data.setMaskMode()}>
+          <button
+            className="tools__button"
+            type="button"
+            onClick={() => data.setMaskMode()}
+          >
             <Icon id="layer-group" width="64" />
             <h3 className="tools__button-title">Маска</h3>
           </button>
@@ -63,7 +79,9 @@ const Tools = observer(({ data }) => {
             className="tools__button"
             type="button"
             disabled={data.steps.length <= 1}
-            onClick={() => (data.steps.length > 1) ? setModalID(MODAL_DELETE_STEP_ID) : null}
+            onClick={() =>
+              data.steps.length > 1 ? setModalID(MODAL_DELETE_STEP_ID) : null
+            }
           >
             <Icon id="trash" width="64" />
             <h3 className="tools__button-title">Удалить слайд</h3>
